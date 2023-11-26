cdb311ab60b64e06ebc264143f046875115a44e7
Akshay Urs <84136865+akshayurs@users.noreply.github.com>
2023-11-26 23:44:22 +0530

    answer length changed

--- src/pages/Rules.tsx
+++ src/pages/Rules.tsx
@@ -6,7 +6,7 @@ const Rules = () => {
         'In case of a tie, the person who has submitted first will be on top',
         "Answers Won't repeat",
         "Don't use any special characters/symbols in the answer(just skip them if they are part of answer)",
-        'Minimum length of answer is 3 and maximum is 30',
+        'Minimum length of answer is 3 and maximum is 40',
         'You can Give answers in either uppercase or lowercase',
         'Spaces in the answer are ignored',
         "For Example, if the answer is 'Linux Campus Club'. All these answers are vaild: 'LinuxCampusClub', 'linuxcampusclub', 'Linux Campus Club', 'linux   campus club'"
